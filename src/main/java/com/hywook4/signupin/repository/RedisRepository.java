69bbe556ddf55ece3fac43e12b178134f1abd471
hyjung05 <hyjung05@coupang.com>
2021-10-30 03:44:12 +0900

    Add Redis config and Redis repository

---
+++ src/main/java/com/hywook4/signupin/repository/RedisRepository.java
@@ -0,0 +1,29 @@
+package com.hywook4.signupin.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.TimeUnit;
+
+@Repository
+public class RedisRepository {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    public RedisRepository(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void insertKeyValue(String key, String value, int ttl) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, value, ttl, TimeUnit.MINUTES);
+    }
+
+    public String getValueByKey(String key) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+}
