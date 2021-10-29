26113d461223afc435e7906a062db480b769757b
hyjung05 <hyjung05@coupang.com>
2021-10-29 18:47:27 +0900

    Initialize project

---
+++ src/main/java/com/hywook4/signupin/SignUpInApplication.java
@@ -0,0 +1,13 @@
+package com.hywook4.signupin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignUpInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignUpInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hywook4/signupin/SignUpInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hywook4.signupin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignUpInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
