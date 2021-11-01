48846aac31c8ac34d47028947414211ffd0d2a14
hyjung05 <hyjung05@coupang.com>
2021-11-02 01:41:48 +0900

    Add init sql for local H2 DB and ignore directory

---
+++ src/main/resources/db/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS user (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` CHAR(32) NOT NULL,
+    `nickname` CHAR(20) NOT NULL,
+    `name` CHAR(10) NOT NULL,
+    `phone_number` CHAR(15) NOT NULL,
+    `password` CHAR(25) NOT NULL,
+    `token` CHAR(50) NOT NULL,
+    PRIMARY KEY(`id`),
+    UNIQUE KEY(`email`),
+    UNIQUE KEY(`phone_number`),
+    UNIQUE KEY(`token`)
+);
