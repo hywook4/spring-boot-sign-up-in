e8e8a7bdbdcb97921785b6f9040d4c0044f19037
hyjung05 <hyjung05@coupang.com>
2021-11-03 00:49:03 +0900

    Remove unnecessary test

--- src/test/java/com/hywook4/signupin/SignUpInApplicationTests.java
+++ src/test/java/com/hywook4/signupin/SignUpInApplicationTests.java
@@ -1,13 +1,8 @@
 package com.hywook4.signupin;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SignUpInApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
